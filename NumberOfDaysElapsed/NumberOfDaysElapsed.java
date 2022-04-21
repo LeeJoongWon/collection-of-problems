56947127a7fa0c2f5c6d43bd8d775a4bf6bf59ba
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-04-21 17:27:51 +0900

    Update NumberOfDaysElapsed.java

--- NumberOfDaysElapsed/NumberOfDaysElapsed.java
+++ NumberOfDaysElapsed/NumberOfDaysElapsed.java
@@ -10,7 +10,7 @@ public class NumberOfDaysElapsed {
 			{31,28,31,30,31,30,31,31,30,31,30,31}	//윤년  mdays[1][]
 	};
 	
-	// 평년 윤년 구분 / 윤년일 1 평년일 0 반환
+	// 평년 윤년 구분 / 윤년 1 평년 0 반환
 	static int isLeep(int year) {
 		return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
 	}
