5bfed181bcc947752e5785f18fb434ed6efc8382
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-06-09 07:52:45 +0900

    Add files via upload

---
+++ 숫자 문자열과 영단어/Solution.java
@@ -0,0 +1,20 @@
+package Test;
+
+class Solution {
+	
+	public static void main(String[] arg) {
+		
+		String txt = "one4seveneight";
+		String[] numberEnglish = {
+	    			"zero","one","two","three","four",
+	    			"five","six","seven","eight","nine"};
+		
+	    for(int i =0; i<numberEnglish.length;i++) {
+	    	txt = txt.replaceAll(numberEnglish[i], Integer.toString(i));
+	    }
+	    
+	    int aws = Integer.parseInt(txt);
+	    System.out.print(aws);
+	    
+	}		
+}
