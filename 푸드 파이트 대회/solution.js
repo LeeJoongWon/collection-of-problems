cdde79a1a764bfe103e6ee3c846ceefdf622514c
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-09 13:13:43 +0900

    Add files via upload

--- 푸드 파이트 대회/solution.js
+++ 푸드 파이트 대회/solution.js
@@ -1,19 +1,18 @@
-function solution(s) {
-    let same = 1,
-        other = 0,
-        count = 1,
-        point = s[0];
-    for (let i = 1; i < s.length; i++) {
-        if (same === other) {
-            point = s[i];
-            same = 1;
-            other = 0;
-            count++;
-        } else if (point === s[i]) {
-            same++;
-        } else {
-            other++;
-        }
+function addFood(arr, num, food) {
+    if (num % 2 === 1) num--;
+    num /= 2;
+    console.log(num);
+    for (let i = 0; i < num; i++) {
+        arr.push(food);
     }
-    return count;
 }
+
+function solution(food) {
+    let arr = [];
+    for (let i = 1; i < food.length; i++) {
+        addFood(arr, food[i], i);
+    }
+    const reversed = [...arr].reverse();
+    return [...arr, 0, ...reversed].join('');
+}
+console.log(solution([1, 7, 1, 2]));
